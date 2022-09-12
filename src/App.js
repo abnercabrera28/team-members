5a678ef7526be8a5c992a48d742d27d5334826a4
Abner <abnercabrera28@gmail.com>
2022-09-12 03:05:18 -0400

    import Header, Content, and Footer files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="mt-4 p5 bg-primary text-white rounded">
-      hello
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
