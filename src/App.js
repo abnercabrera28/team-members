93914e0aa2abed3ac44e37552b8bc63876c779c0
Abner <abnercabrera28@gmail.com>
2022-09-12 22:30:09 -0400

    delete Content file and remove import from App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Header from "./Header";
-import Content from "./Content";
 import Footer from "./Footer";
 
 function App() {
   return (
     <div className="mt-4 p5 bg-primary text-white rounded">
       <Header />
-      <Content />
+      
       <Footer />
     </div>
   );
