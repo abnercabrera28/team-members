6f2c22e024647bf780f8c85dd56cf94e99cea82b
Abner <abnercabrera28@gmail.com>
2022-09-12 22:32:17 -0400

    import Employees

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Employees from "./Employees";
 
 function App() {
   return (
     <div className="mt-4 p5 bg-primary text-white rounded">
       <Header />
-      
+      <Employees />
       <Footer />
     </div>
   );
