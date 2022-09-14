093b0286da79de66cb847e48184f9f5492bb48b0
Abner <abnercabrera28@gmail.com>
2022-09-13 22:27:42 -0400

    add ternary to change img based on gender

--- src/Employees.js
+++ src/Employees.js
@@ -99,7 +99,9 @@ const Employees = () => {
                     {
                         employees.map(employee => (
                             <div id={employee.id} className="card m-2" style={{cursor: "pointer"}}>
-                                <img src={femaleProfile} className="card-img-top" />
+                                {employee.gender === "male" ?
+                                <img src={maleProfile} className="card-img-top" />
+                                :<img src={femaleProfile} className="card-img-top" />}
                                 <div className='card-body'>
                                     <h5 className='card-title'>Full Name: {employee.fullName}</h5>
                                     <p className='card-text'><b>Designation: {employee.designation}</b></p>
