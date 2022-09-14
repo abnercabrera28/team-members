466f308f4e84c2ceba27c1e2573a796418443593
Abner <abnercabrera28@gmail.com>
2022-09-13 22:50:10 -0400

    add form select to make dropdown larger

--- src/Employees.js
+++ src/Employees.js
@@ -95,7 +95,7 @@ const Employees = () => {
         <div className='container'>
             <div className='row justify-content-center mt-3 mb-3'>
                 <div className='col-6'>
-                    <select>
+                    <select className='form-select form-select-lg'>
                         <option value="TeamA">TeamA</option>
                         <option value="TeamB">TeamB</option>
                         <option value="TeamC">TeamC</option>
