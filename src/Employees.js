2c16a18222e9ef8127d5cf994ea803b1a38bae48
Abner <abnercabrera28@gmail.com>
2022-09-15 14:34:07 -0400

    create handleSelectedTeam function

--- src/Employees.js
+++ src/Employees.js
@@ -93,11 +93,15 @@ const Employees = () => {
         teamName: "TeamD"
       }])
 
+      const handleSelectedTeam = (event) => {
+        setTeam(event.target.value)
+      }
+
     return (
         <div className='container'>
             <div className='row justify-content-center mt-3 mb-3'>
                 <div className='col-6'>
-                    <select className='form-select form-select-lg' value={selectedTeam}>
+                    <select className='form-select form-select-lg' value={selectedTeam} onChange={handleSelectedTeam}>
                         <option value="TeamA">TeamA</option>
                         <option value="TeamB">TeamB</option>
                         <option value="TeamC">TeamC</option>
